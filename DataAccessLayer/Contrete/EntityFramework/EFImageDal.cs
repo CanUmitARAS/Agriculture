a11b26aeafd5afc1efac78ac2df3f00889bc7dfc
ümit aras <umit.aras96@gmail.com>
2023-06-27 23:41:59 +0300

    Add project files.

---
+++ DataAccessLayer/Contrete/EntityFramework/EFImageDal.cs
@@ -0,0 +1,15 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Contrete.Repository;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Contrete.EntityFramework
+{
+    public class EFImageDal:GenericRepository<Image>, IImageDal
+    {
+    }
+}
